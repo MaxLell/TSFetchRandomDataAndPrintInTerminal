320c909cc311e25f66a58d8c2d0676406e11224d
Max Lell <maximilian.lell@gmail.com>
2023-10-27 05:00:46 +0100

    added some nicer formating and an intentional bug

--- fetchJson/index.ts
+++ fetchJson/index.ts
@@ -5,5 +5,15 @@ const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
 // HTTP Request to fetch the data
 axios.get(url).then(response => {
-    console.log(response.data);
+    const todo = response.data;
+    
+    const id = todo.ID;
+    const title = todo.Title;
+    const finished = todo.finished;
+
+    console.log(`
+        The Todo with ID ${id}
+        Has a title of: ${title}
+        Is it finished? ${(finished) ? "Yep" : "No"}
+    `)
 });
